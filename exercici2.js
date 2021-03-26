ca72c11d5fba5ea8b8d64b0248c5a9320fda44eb
MarinaGrifell <marinagrifell@hotmail.com>
2021-03-26 15:29:24 +0100

    create exercici2

---
+++ exercici2.js
@@ -0,0 +1,23 @@
+/* Arregla l'error del següent bloc de codi: */
+
+/* users.map(function (user) {
+   user.firstName;
+});
+
+var users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }]; */
+
+users.map((user)=> {
+   user.firstName;
+});
+
+let users = 
+[{ firstName: 'Homer', lastName: 'Simpson' },
+{ firstName: 'Marge', lastName: 'Simpson' },
+{ firstName: 'Bart', lastName: 'Simpson' },
+{ firstName: 'Lisa', lastName: 'Simpson' },
+{ firstName: 'Maggie', lastName: 'Simpson' }];
